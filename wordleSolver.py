ccf8eac1dafef8aa0dce1c225bf5eb0f67447d0b
RephlexZero <rephlexzero@protonmail.com>
2023-12-31 03:51:03 +0000

    Recursive entropy

--- wordleSolver.py
+++ wordleSolver.py
@@ -33,33 +33,35 @@ def letters_in_correct_positions(guess, word):
 desired_length = 5
 words = [word.lower() for word in words.words() if len(word) == desired_length]
 
-# Get frequency distribution of letters in the dictionary words
-letter_freq = Counter("".join(words))
+def calculate_entropy(words):
+    # Get frequency distribution of letters in the words
+    letter_freq = Counter("".join(words))
 
-# Calculate the probability of each letter in the words
-letter_probabilities = {letter: letter_freq[letter] / sum(letter_freq.values()) for letter in letter_freq}
+    # Calculate the probability of each letter in the words
+    letter_probabilities = {letter: letter_freq[letter] / sum(letter_freq.values()) for letter in letter_freq}
 
-def calculate_entropy(word):
-    entropy = -sum(letter_probabilities[letter] * math.log(letter_probabilities[letter], 2) for letter in set(word))
-    return entropy
+    word_entropies = {}  # Create an empty dictionary to store word entropies
 
-# Calculate entropy for each word
-word_entropies = {word: calculate_entropy(word) for word in words}
-# Sort words by entropy in descending order
-sorted_words = sorted(word_entropies.items(), key=lambda x: x[1], reverse=True)
+    for word in words:
+        entropy = -sum(letter_probabilities[letter] * math.log(letter_probabilities[letter], 2) for letter in set(word))
+        word_entropies[word] = entropy  # Add word entropy to the dictionary
+
+    return word_entropies
 
 guesses_to_success = []
 # Guess loop
 while True:
-    guessing_words = sorted_words.copy()
-    random_word = random.choice(guessing_words)[0]
+    guessing_words = words.copy()
+    random_word = random.choice(guessing_words)
     print("Random word:", random_word)
 
     guesses_taken = 0
     while True:
-        highest_entropy_word = guessing_words[0][0]
+        # Recalculate entropy for each word
+        guessing_words = calculate_entropy(guessing_words)      
+        highest_entropy_word = max(guessing_words, key=guessing_words.get)
         print("Length of guessing set:", len(guessing_words))
-        print("Guessed:", highest_entropy_word)
+        print("Guessed:", highest_entropy_word, "with entropy", guessing_words[highest_entropy_word])
         guesses_taken += 1
         
         # Update guess logic here
@@ -67,15 +69,15 @@ while True:
         if highest_entropy_word == random_word:
             break
         else:
-            guessing_words.pop(0)
+            guessing_words.pop(highest_entropy_word)
             for letter in letters_not_in_word(highest_entropy_word, random_word):
-                guessing_words = [word for word in guessing_words if letter not in word[0]]
+                guessing_words = [word for word in guessing_words if letter not in word]
 
             for i, letter in letters_in_incorrect_positions(highest_entropy_word, random_word):
-                guessing_words = [word for word in guessing_words if letter not in word[0][i]]
+                guessing_words = [word for word in guessing_words if letter not in word[i]]
 
             for i, letter in letters_in_correct_positions(highest_entropy_word, random_word):
-                guessing_words = [word for word in guessing_words if word[0][i] == letter]
+                guessing_words = [word for word in guessing_words if word[i] == letter]
 
     print("Guesses taken:", guesses_taken)
     guesses_to_success.append(guesses_taken)
