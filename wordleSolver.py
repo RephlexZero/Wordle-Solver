33e14cca0cd601b8b8f987d81d37a85eba1da7c3
RephlexZero <rephlexzero@protonmail.com>
2024-02-16 00:53:58 +0000

    Improvements

--- wordleSolver.py
+++ wordleSolver.py
@@ -1,17 +1,16 @@
 import nltk
 from collections import Counter
-
-nltk.download('words')
-
-from nltk.corpus import words
 import random
 import math
 
-def calculate_letter_probability(words, word_length):
+# Assuming nltk has been downloaded and setup
+from nltk.corpus import words
+
+def calculate_letter_probability(word_list, word_length):
     letter_frequency = [Counter() for _ in range(word_length)]
     letter_probability = [Counter() for _ in range(word_length)]
 
-    for word in words:
+    for word in word_list:
         for i, letter in enumerate(word):
             letter_frequency[i][letter] += 1
 
@@ -22,63 +21,67 @@ def calculate_letter_probability(words, word_length):
 
     return letter_probability
 
-
-def calculate_word_entropy(word, letter_probability):
+def calculate_word_entropy(word, letter_probability, word_length):
     entropy = 0
+    letter_seen = set()
     for i, letter in enumerate(word):
-        probability = letter_probability[i][letter] / word.count(letter)
-        entropy += -probability * math.log2(probability)
+        if letter not in letter_seen:
+            probability = letter_probability[i][letter]
+            entropy += -probability * math.log2(probability)
+            letter_seen.add(letter)
     return entropy
 
-def calculate_word_entropy_dict(words, letter_probability):
-    word_entropy_dict = {}
-
-    for word in words:
-        entropy = calculate_word_entropy(word, letter_probability)
-        word_entropy_dict[word] = entropy
-
-    return word_entropy_dict
-
 def cull_word_list(word_list, guessed_word, target_word):
-    for i, letter in enumerate(guessed_word):
-        if letter == target_word[i]:
-            word_list = [word for word in word_list if word[i] == letter]
-        elif letter in target_word:
-            word_list = [word for word in word_list if word[i] != letter]
-        elif letter not in target_word:
-            word_list = [word for word in word_list if letter not in word]
-    return word_list
+    culled_word_list = []
+    for word in word_list:
+        match = True
+        for i, letter in enumerate(guessed_word):
+            if letter == target_word[i]:
+                if word[i] != letter:
+                    match = False
+                    break
+            elif letter in target_word:
+                if word[i] == letter or letter not in word:
+                    match = False
+                    break
+            else:  # letter not in target_word
+                if letter in word:
+                    match = False
+                    break
+        if match:
+            culled_word_list.append(word)
+    return culled_word_list
 
 def guess_highest_entropy_word(word_entropy_dict):
     return max(word_entropy_dict, key=word_entropy_dict.get)
 
-def __main__(word_length=5, word_list_length=900):
+def main(word_length=5, word_list_length=900):
     word_list = [word.lower() for word in words.words() if len(word) == word_length]
-    # Make the list of length k with random words, real wordle is k = 900
-    word_list = random.choices(word_list, k=word_list_length)
-    # Choose a random word from the list
+    word_list = random.sample(word_list, k=word_list_length)  # Use sample to avoid duplicates
     target_word = random.choice(word_list)
 
     letter_probability = calculate_letter_probability(word_list, word_length)
-    word_entropy_dict = calculate_word_entropy_dict(word_list, letter_probability)
+    
+    word_entropy_dict = {word: calculate_word_entropy(word, letter_probability, word_length) for word in word_list}
     
     guesses = 0
     print("Guess:\tList Size:")
-    while (True):
+    while True:
         guesses += 1
         guessed_word = guess_highest_entropy_word(word_entropy_dict)
-        print(guessed_word,"\t", len(word_list))
+        print(guessed_word, "\t", len(word_list))
         if guessed_word == target_word:
             print("Correct! In", guesses, "guesses.")
-            return guesses
+            break
         word_list = cull_word_list(word_list, guessed_word, target_word)
-        # letter_probability = calculate_letter_probability(word_list, word_length)
-        word_entropy_dict = calculate_word_entropy_dict(word_list, letter_probability)
+        # Update entropy only for remaining words, using pre-calculated values
+        word_entropy_dict = {word: word_entropy_dict[word] for word in word_list}
 
+    return guesses
+
+# For testing and averaging
 total_guesses = 0
-for i in range(100):
-    total_guesses += __main__()
-    print("Average:", round(total_guesses / (i + 1), 2),"\n")
- 
-
-
+n_runs = 100
+for i in range(n_runs):
+    total_guesses += main()
+    print("Average after", i + 1, "runs:", round(total_guesses / (i + 1), 2), "\n")
